e5fb7d6d69b4413ae400dc75e0853e9cba0bf76e
Edwin Mayr <edwin@gmail.com>
2025-09-11 14:59:25 -0300

    mudanca

--- Script/SCD_Edwin.R
+++ Script/SCD_Edwin.R
@@ -7,16 +7,29 @@ head(Patient)
 
 #from here on i will organize the data
 install.packages("janitor")
+install.packages("GGally")
 library(tidyverse)
 library(janitor)
 library(dplyr)
-
-#Here i selected only the parameters i could have any interest in studying
+library(GGally)
+#Here i selected only the parameters i could have any interest in studying in my study
 names(Patient)
-Patient <- Patient %>% select (ID, Initials, Registry, Sample_date, Birthdate, Group_pt_HI, Age, Sex, Genotype, VCM, Plat, `DD hemostasia (ng/mL)`, `P-selectin`, uPAR, TGT_Lagtime, TGT_ETP, TGT_Peak, TGT_tt_Peak)
+Patient_alldata <- Patient %>% select (ID, Initials, Registry, Sample_date, Birthdate, Group_pt_HI, Age, Sex, Genotype, `Hb F`, LDH, `Bilirrubina total`, VCM, RDW, VPM, Hb, WBC, Seg, Lymph, Mono, Eos, Plat, Retc, `DD hemostasia (ng/mL)`, `FVW:Ag`, `FVW Activity (%)`, TGT_Lagtime, TGT_ETP, TGT_Peak, TGT_tt_Peak)
 
 #Here i sliced the empty rows to only keep the rows with patients and controls (Alternatively i could have done: Patient1 <- Patient %>% filter(!is.na(Sample_date) & Sample_date != "")) to exclude rows without Sample Date
-Patient_1 <- Patient %>% slice(1:76)
+Patient_alldataclean <- Patient_alldata %>% slice(1:76)
 
 #Here i will remove the row that was empty and used as a space
-Patient_1 <- Patient_1 %>% slice(-51)
+Patient_alldataclean <- Patient_alldataclean %>% slice(-51)
+
+#Here i selected only the parameters i could have any interest in studying but i removed non numeric values to calculate correlation
+names(Patient)
+Patient_correl <- Patient_alldataclean %>% select (Age,`Hb F`, LDH, `Bilirrubina total`, VCM, RDW, VPM, Hb, WBC, Seg, Lymph, Mono, Eos, Plat, Retc, `DD hemostasia (ng/mL)`, `FVW:Ag`, `FVW Activity (%)`, TGT_Lagtime, TGT_ETP, TGT_Peak, TGT_tt_Peak)
+
+#Here i will do a pearson correlation using the GGally package for visualization
+ggpairs(Patient_correl,
+        upper = list(continuous = wrap("cor", size = 3)),  # Pearson correlation values
+        lower = list(continuous = "smooth"),               # scatterplots with regression line
+        diag  = list(continuous = "densityDiag"))          # density plots on the diagonal
+w
+
